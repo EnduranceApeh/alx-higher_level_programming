ec0b3f7344ef38822d102a7621c6b5fa2e1b2557
EnduranceApeh <apehendurance133@gmail.com>
2023-11-30 14:38:33 -0800

    modify code in task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
-arguements = len(sys.argv[1:])
+arguements = sys.argv[1:]
 if __name__ == "__main__":
-    if (arguements > 1):
-        print("{} arguments: ".format(arguements))
+    if (len(arguements) > 1):
+        print(f"{len(arguements)} arguments: ")
     elif (len(arguements) == 0):
-        print("{} arguments. ".format(len(arguements)))
+        print(f"{len(arguements)} arguments. ")
     else:
-        print("{} argument: ".format(len(arguements)))
+        print(f"{len(arguements)} argument: ")
     # iterate through the arguement and print by index starting from index 1
     length = len(arguements)
     for index in range(length):
         arg = 1 + index
-        print("{}: {}".format(arg, arguements[index]))
+        print(f"{arg}: {arguements[index]}")
