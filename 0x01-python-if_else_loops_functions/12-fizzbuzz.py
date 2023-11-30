66f2388d2dd68f641ede781570c810b4fb8dc854
EnduranceApeh <apehendurance133@gmail.com>
2023-11-29 20:43:07 -0800

    modify code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for num in range(1, 101):
         if (num % 3 == 0 and num % 5 == 0):
             print("FizzBuzz", end=" ")
-        if (num % 3) == 0:
+        if (num % 3 == 0):
             print("Fizz", end=" ")
         if (num % 5 == 0):
             print("Buzz", end=" ")
