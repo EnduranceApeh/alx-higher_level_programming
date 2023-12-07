76e0dfc4f681e54146cc4cc7568263fb8c6984d5
EnduranceApeh <apehendurance133@gmail.com>
2023-12-06 16:46:03 -0800

    Modify code if file 6 and 7

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is None:
-        return 
+        return
     # print dictionary by ordered key
     sorted_keys = sorted(a_dictionary.keys())
     for key in sorted_keys:
