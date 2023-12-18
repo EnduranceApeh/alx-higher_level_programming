20cc24cb04a9626b569d3b1a32ed199b6e41ab05
EnduranceApeh <apehendurance133@gmail.com>
2023-12-18 10:28:31 -0800

    Task 0 done

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    element_count = 0
+    index = 0
+    try:
+        while index < x:
+            print(my_list[index],end="")
+            index += 1
+            element_count += 1
+    except:
+        pass
+    print()
+    return element_count
