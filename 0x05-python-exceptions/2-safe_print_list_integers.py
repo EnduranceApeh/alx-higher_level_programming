a22cb64526eda53fe59aa5e83cac4a3317c7adae
EnduranceApeh <apehendurance133@gmail.com>
2023-12-19 16:51:20 -0800

    Debug code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         while index < x:
             if type(my_list[index]) == int:
-                print("{:d}".format(my_list[index]),end="")
+                print("{:d}".format(my_list[index]), end="")
                 index += 1
                 element_count += 1
             else:
