468a5f284678d81a2f410eb314d04dbbfc624f0c
EnduranceApeh <apehendurance133@gmail.com>
2024-01-01 18:24:30 -0800

    Task 1 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Define class Square
+"""
+
+
+class Square:
+    """class Square definition"""
+
+    def __init__(self, size):
+        """
+        Attribute:
+            size: size of a side of a square
+        """
+        self.__size = size
