feacb1afdaab4583547a6cb33b0c088fbd985189
EnduranceApeh <apehendurance133@gmail.com>
2024-01-10 03:59:10 -0800

    Commit chnage

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,12 @@ write string to text file and return num of character
 
 
 def write_file(filename="", text=""):
+    """
+    This function write a 
+    string to a text and 
+    return the number of character
+    """
+
     with open(filename, 'w', encoding="utf-8") as rf:
-        return rf.write(text)
+       num = rf.write(text)
+       return num
