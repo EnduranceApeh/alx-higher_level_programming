13d315788f6339980457f443838e68c3f310a063
EnduranceApeh <apehendurance133@gmail.com>
2024-01-09 16:24:52 -0800

    Task 0 done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 1-write_file
+This function read text and print to stdout
+"""
+
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
+        f.close()
