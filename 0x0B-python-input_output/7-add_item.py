11e0d3df77b45d4530f8ee69abf4caadf475f096
EnduranceApeh <apehendurance133@gmail.com>
2024-01-10 09:52:12 -0800

    Mdofiy code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,5 +12,5 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 # Add all args to a list
 args_list = sys.argv[1:]
-my_list = save_to_json_file(args_list, "add_item.json")
 load_from_json_file("add_item.json")
+save_to_json_file(args_list, "add_item.json")
