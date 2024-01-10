b3744bf5f0a3b2d305aef9d4ea0f72dc66d6385d
EnduranceApeh <apehendurance133@gmail.com>
2024-01-10 05:50:27 -0800

    task 3 done

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+Module 3-to_json_string
+This function retun a JSON representation of an object
+"""
+
+def to_json_string(my_obj):
+    """Return JSON representaion on an object"""
+
+    json_str = json.dumps(my_obj)
+    return json_str
