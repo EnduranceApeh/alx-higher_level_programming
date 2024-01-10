63be476fc2d4d00ed6fe687a86d1be01dff619b2
EnduranceApeh <apehendurance133@gmail.com>
2024-01-10 07:55:39 -0800

    commit chnage

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 6-load_from_json_file
+This function create an object from JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """Create an object from JSON file"""
+
+    with open(filename, encoding="utf-8") as my_obj:
+        json.load(my_obj)
+
