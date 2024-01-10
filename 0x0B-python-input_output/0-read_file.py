6af92d3ae218292e66d466efa08f6058a498cc4c
EnduranceApeh <apehendurance133@gmail.com>
2024-01-09 16:40:28 -0800

    modify codee

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ This function read text and print to stdout
 
 
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end='')
         f.close()
