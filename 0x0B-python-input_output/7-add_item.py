15e8ab38c2b732cf00f76982996e9673682e8255
EnduranceApeh <apehendurance133@gmail.com>
2024-01-10 16:30:53 -0800

    Add chan

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,9 +11,14 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 # Add all args to list
-args_list = sys.argv[1:]
 filename = "add_item.json"
-my_list = load_from_json_file(filename)
+try:
+    my_list = load_from_json_file(filename)
+except Exception:
+    my_list = []
+    save_to_json_file(my_list, "add_item.json")
+
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
+
 save_to_json_file(my_list, filename)
