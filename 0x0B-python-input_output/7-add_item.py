1d8a8e1d8566a8dbe5350991d171ece833b28fe9
EnduranceApeh <apehendurance133@gmail.com>
2024-01-10 16:16:58 -0800

    Modify ce

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,7 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+# Add all args to list
 args_list = sys.argv[1:]
 filename = "add_item.json"
 my_list = load_from_json_file(filename)
