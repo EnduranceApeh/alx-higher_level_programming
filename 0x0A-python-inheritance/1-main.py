d75b9b2b2fa41a0eee393de7725a04852ec8954b
EnduranceApeh <apehendurance133@gmail.com>
2024-01-13 07:46:02 -0800

    Task 2 done

---
+++ 0x0A-python-inheritance/1-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList()
+my_list.append(-34)
+my_list.append(-21)
+my_list.append(-12)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+A class that inherit from list
+print sorted list
+"""
+
+class MyList(list):
+    """
+    Inherit from list
+
+    method:
+        print_sorted (self)
+    """
+
+    def print_sorted(self):
+        print(sorted(self))
