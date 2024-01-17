3b6d5ce1d4861ea3df56e14b99f9d4ab411782a4
EnduranceApeh <apehendurance133@gmail.com>
2024-01-17 04:07:52 -0800

    Task 11 done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,6 +25,16 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+    @size.setter
+    def size(self, value):
+        """setter for size, set value of wodth and height to size"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a string"""
         return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
