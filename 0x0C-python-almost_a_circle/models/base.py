895adfcca07bba016e933de6d75ab1c88b03f515
EnduranceApeh <apehendurance133@gmail.com>
2024-01-19 04:24:28 -0800

    Task 15 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ Module contain class base base
 
 contain private class __nb_objects, and constructor __init__
 """
+import json
 
 
 class Base:
@@ -13,6 +14,7 @@ class Base:
         __nb_objects
     Methods:
         __init__(self, id=None)
+        to_json_string(list_dictionaries)
     """
     __nb_objects = 0
 
@@ -27,3 +29,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return json string rep of python dict"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            json_str = json.dumps(list_dictionaries)
+            return json_str
