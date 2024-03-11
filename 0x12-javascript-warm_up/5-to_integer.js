f56c68b46f4c8337f5188bf7bfb1148ca987a443
EnduranceApeh <apehendurance133@gmail.com>
2024-03-11 13:55:48 -0700

    Task 5 done

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// convert string to integer and print
+
+const argument = process.argv[2];
+const number = parseInt(argument);
+
+if (!isNaN(number)) {
+  console.log('My number: ', number);
+} else {
+  console.log('Not a number');
+}
