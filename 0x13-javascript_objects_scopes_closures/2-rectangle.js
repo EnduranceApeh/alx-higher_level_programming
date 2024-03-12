019aeaf29a04bfc56bf25f8abff4581d0e499daa
EnduranceApeh <apehendurance133@gmail.com>
2024-03-12 11:50:21 -0700

    Task 2 done

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,6 +3,9 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return {};
+    }
     this.width = w;
     this.height = h;
   }
