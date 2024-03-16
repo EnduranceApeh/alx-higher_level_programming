c56df2b0c2e43f74922cc3bfda03d2e4882eee1c
EnduranceApeh <apehendurance133@gmail.com>
2024-03-16 16:05:33 -0700

    Task 2 done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a script that list all state with a name startinmg with N
+paThis script recieve three paarameterer: udername, passd and database name
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            database=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(argv[4]))
+    states = cursor
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
