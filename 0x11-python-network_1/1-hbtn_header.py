fabec821e048990feeffcb15444e58834f1625fb
EnduranceApeh <apehendurance133@gmail.com>
2024-03-28 08:51:17 -0700

    Task 1 done

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+displays the value of the X-Request-Id variable
+"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.getheader('X-Request-Id')
+        print(header)
